af73c3bcccf11b569ac90c3055d46a845085d282
Junior Gomes <josivaldo.ifpb@gmail.com>
2022-10-06 16:58:23 -0300

    1003

---
+++ Beecrowd/src/Q1003.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Q1003 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("SOMA = "+(a+b));
+	}
+
+}
