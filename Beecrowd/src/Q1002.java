a63a9328f767f5343a421f3c0ed6dd16711a8dce
Junior Gomes <josivaldo.ifpb@gmail.com>
2022-10-06 16:51:45 -0300

    1002

---
+++ Beecrowd/src/Q1002.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class Q1002 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		double area;
+		double r = sc.nextDouble();
+		area = 3.14159 * (r*r);
+		System.out.println("A = "+String.format("%.4f", area));
+	}
+
+}
